dec548f1064a96aadbd4fd43b1b8e0469fa3fe8d
Cyr <cyr@zolotarev.pp.ua>
2019-10-31 01:20:49 +0200

    models admin

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
+from django.shortcuts import render
+from core.models import Card
+from django.shortcuts import get_object_or_404
+
 
 def index(request):
-    return render(request, "/core/index.html")
+    ctx = {'product_list': Card.objects.all()}
+    return render(request, 'core/index.html', ctx)
